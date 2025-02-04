b896c2df19cab869bf4e0c4eee930881b598423f
blessings-ng <blessingsigwe02@gmail.com>
2025-02-04 14:48:16 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+function cod() {
+    // Declaring and Assigning Variables
+    let x = 'Hello';
+    let y = ' ';
+    let z = 'World';
+    let a;
+
+    a = x + y + z;
+
+    console.log(a);
+}
+
+code = () => {
+    let a = 3;
+    let b = 4;
+    let c = 5;
+    let d;
+ m
+    d = a + b * c;
+
+    console.log(d);
+}
+
+function dol() {
+    document.getElementById('richie').innerHTML = 'Pablo and Richie';
+    document.getElementById('mary').innerHTML = 'mary has a little lamb';
+    document.getElementsByClassName('rayden').innerHTML = 'Netherrealm is under attack. SOS!';
+}
+
+// function oblee() {
+//     document.getElementById('thor').href = 'assets/css/dark.css';
+// }
+
+// function mara() {
+//     document.getElementById('thor').href = 'assets/css/light.css';
+// }
+
+function yinyang() {
+    document.getElementById('imole').innerHTML = '<button onclick="yinyang()"><i class="fa-solid fa-moon" style="font-size: 30px; color: crimson;"></i></button>';
+    document.getElementById('thor').href = 'assets/css/dark.css';
+}
+
+function lite() {
+    document.getElementById('yin').style.display = 'none';
+    document.getElementById('yang').style.display = 'inline';
+    document.getElementById('themestyle').href = 'assets/css/light.css';
+}
+
+function nite() {
+    document.getElementById('yang').style.display = 'none';
+    document.getElementById('yin').style.display = 'inline';
+    document.getElementById('themestyle').href = 'assets/css/dark.css';
+}
+
+console.log('');
